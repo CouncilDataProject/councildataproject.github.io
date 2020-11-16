379ea201f9240018721ba44cb4d00c6c8ea91787
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-11-15 20:24:27 -0800

    Nav impl

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import '@mozilla-protocol/core/protocol/css/protocol.css';
+import '@mozilla-protocol/core/protocol/css/protocol-extra.css';
 import ReactGA from 'react-ga';
 
 // Run Google Analytics
@@ -6,20 +8,44 @@ ReactGA.initialize('UA-108611658-2');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        // Header nav
+        <div class="mzp-c-navigation">
+            <div class="mzp-c-navigation-l-content">
+                <div class="mzp-c-navigation-container">
+                    <div class="mzp-c-navigation-menu">
+                        <nav class="mzp-c-menu mzp-is-basic">
+                            <ul class="mzp-c-menu-category-list">
+                                <li class="mzp-c-menu-category">
+                                    <a class="mzp-c-menu-title" href="/news">News</a>
+                                </li>
+                                <li class="mzp-c-menu-category">
+                                    <a class="mzp-c-menu-title" href="/partners">Partners</a>
+                                </li>
+                                <li class="mzp-c-menu-category">
+                                  <a class="mzp-c-menu-title" href="/community">Community</a>
+                                </li>
+                                <li class="mzp-c-menu-category">
+                                  <a class="mzp-c-menu-title" href="/about">About</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        // Hero
+
+        // About
+
+        // Instances
+
+        // FAQ
+
+        // Other Content
+
+        // Footer
     </div>
   );
 }
