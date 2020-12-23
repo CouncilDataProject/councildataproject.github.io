435da8c3eb8bf2136e795464bb14b3b5138b22f5
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2020-12-22 20:48:17 -0800

    admin/contribution-section-and-cleanup (#14)
    * Update logos with transparent and background white
    
    * Update index.html metadata
    
    * Update license year
    
    * Add contributing content and cleanup the rest

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
                 <ul className="mzp-c-menu-category-list">
                   <li className="mzp-c-menu-category">
                     <a className="mzp-c-menu-title" href="#top">
-                      <img src="favicon.png" alt="CDP logo" width="38" />
+                      <img
+                        src="favicon-transparent.png"
+                        alt="CDP logo"
+                        width="38"
+                      />
                     </a>
                   </li>
                   <li className="mzp-c-menu-category">
@@ -42,13 +46,8 @@ function App() {
                     </a>
                   </li>
                   <li className="mzp-c-menu-category">
-                    <a className="mzp-c-menu-title" href="#community">
-                      Community
-                    </a>
-                  </li>
-                  <li className="mzp-c-menu-category">
-                    <a className="mzp-c-menu-title" href="#faq">
-                      FAQ
+                    <a className="mzp-c-menu-title" href="#contributing">
+                      Contributing
                     </a>
                   </li>
                 </ul>
@@ -73,8 +72,9 @@ function App() {
                   follow your local municipality's council action.
                   <br />
                   <br />
-                  Videos, transcripts, voting records, legislation, all
-                  searchable under a single website.
+                  Videos, transcripts, voting records, and legislation
+                  <br />
+                  -- all searchable under a single website.
                 </p>
               </div>
             </div>
@@ -104,13 +104,6 @@ function App() {
               meetings and actions, CDP ensures that everyone is empowered to
               participate in local government.
             </p>
-            <p>
-              CDP is entirely free and open-source. We encourage anyone and
-              everyone to deploy CDP in your own city, county, or however else
-              you can imagine using it. We also welcome{" "}
-              <a href="#community">contribution</a> to the tooling that makes
-              each instance of CDP work.
-            </p>
 
             <h3>How It Works</h3>
             <p>
@@ -126,12 +119,11 @@ function App() {
                 <span className="step-description">
                   <strong>Gather Events</strong> The only requirement for new
                   CDP instance maintainers is to provide a method to gather some
-                  basic event information. Event information can be as simple
-                  as&nbsp;
+                  basic event information. Event information can be as simple as{" "}
                   <a href="https://councildataproject.github.io/cdp-backend/event_data_ingestion_model.html#minimal-event-data">
                     a URL to a meeting recording
-                  </a>
-                  &nbsp;or as expansive as&nbsp;
+                  </a>{" "}
+                  or as expansive as{" "}
                   <a href="https://councildataproject.github.io/cdp-backend/event_data_ingestion_model.html#expanded-event-data-with-event-minutes-items">
                     having all minutes items and vote information attached
                   </a>
@@ -190,6 +182,123 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div id="contributing" className="cdp-bg-yellow">
+          <div className="mzp-l-content mzp-t-content-xl">
+            <article className="mzp-c-article">
+              <h1 className="mzp-c-article-title">Contributing</h1>
+              <p>
+                As an entirely free and open-source project, Council Data
+                Project welcomes contribution in all forms. Want to work on
+                graphic, visual, or UX design, front-end or back-end
+                development, statistical modeling, or project management -- we
+                can use all the help we can get.
+              </p>
+              <p>
+                If you would like to contribute in any of these areas, please
+                reach out on{" "}
+                <a href="https://www.democracylab.org/index/?section=AboutProject&id=145">
+                  DemocracyLab
+                </a>{" "}
+                or simply start tackling issues in one of our{" "}
+                <a href="https://github.com/CouncilDataProject">
+                  GitHub repositories
+                </a>
+                .
+              </p>
+              <dl class="mzp-u-list-styled">
+                <dt>
+                  <a href="https://github.com/CouncilDataProject/cdp-backend">
+                    cdp-backend
+                  </a>
+                </dt>
+                <dd>
+                  Contains all database models, data processing pipelines, and
+                  infrastructure-as-code for CDP deployments. Contributions here
+                  will be available to all CDP instances. Entirely written in
+                  Python.
+                </dd>
+                <dt>
+                  <a href="https://github.com/CouncilDataProject/cdp-frontend">
+                    cdp-frontend
+                  </a>
+                </dt>
+                <dd>
+                  Contains all of the components used by the web apps to be
+                  hosted on GitHub Pages. Contributions here will be available
+                  to all CDP instances. Entirely written in TypeScript and
+                  React.
+                </dd>
+                <dt>
+                  <a href="https://github.com/CouncilDataProject/cookiecutter-cdp-deployment">
+                    cookiecutter-cdp-deployment
+                  </a>
+                </dt>
+                <dd>
+                  A template to be used by the Python{" "}
+                  <a href="https://github.com/cookiecutter/cookiecutter">
+                    cookiecutter
+                  </a>{" "}
+                  package to create an entirely new deployment repository. This
+                  is where <em>cdp-backend</em> and <em>cdp-frontend</em> are
+                  imported and used. If you would like to create a new
+                  deployment under the <em>councildataproject</em> domain please
+                  log a{" "}
+                  <a href="https://github.com/CouncilDataProject/councildataproject.github.io/issues">
+                    GitHub issue
+                  </a>
+                  .
+                </dd>
+                <dt>
+                  <a href="https://github.com/CouncilDataProject/councildataproject.github.io">
+                    councildataproject.github.io
+                  </a>
+                </dt>
+                <dd>
+                  This website! Contributions here should largely be text
+                  changes and admin updates.
+                </dd>
+              </dl>
+              <h3>Contributors</h3>
+              <p>
+                Council Data Project was created by{" "}
+                <a href="https://jacksonmaxfield.github.io/">
+                  Jackson Maxfield Brown
+                </a>{" "}
+                and <a href="http://nicweber.info/">Nic Weber</a>.
+              </p>
+              <p>
+                There have been major contributions from:{" "}
+                <a href="https://github.com/tohuynh">To Huynh</a> (Full Stack
+                Development), <a href="https://www.isaacna.com/">Isaac Na</a>{" "}
+                (Backend Development),{" "}
+                <a href="https://www.katlynmfgreene.com/">Katlyn M.F. Greene</a>{" "}
+                (UX Research),{" "}
+                <a href="https://www.emilygilles.design/">Emily Gilles</a> (UX
+                Design), and{" "}
+                <a href="https://www.linkedin.com/in/madeleine-farrer-14a1a3118/">
+                  Madeleine Farrer
+                </a>{" "}
+                (Logo Design).
+              </p>
+              <p>
+                Much of our second prototype can be attributed to the voluteers
+                from{" "}
+                <a href="https://medium.com/democracylab-org/textio-engages-employees-to-accelerate-civic-innovation-5984609a95ce">
+                  Textio
+                </a>
+                .
+              </p>
+              <p>
+                Many volunteers and contributors have found the project through{" "}
+                <a href="https://www.democracylab.org/index/?section=Home">
+                  DemocracyLab
+                </a>
+                .
+              </p>
+            </article>
+          </div>
+        </div>
       </main>
 
       <footer className="mzp-c-footer">
@@ -197,7 +306,7 @@ function App() {
           <nav className="mzp-c-footer-secondary">
             <div className="mzp-c-footer-legal">
               <p>
-                Portions of this content are ©2017–2020 by individual Council
+                Portions of this content are ©2017–2021 by individual Council
                 Data Project contributors. Content available under{" "}
                 <a
                   rel="license"
