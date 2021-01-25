3ed40929e7f60648fec68aca729e0a9701c7ea8b
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-01-24 16:18:54 -0800

    Use cdp-logo in css in nav

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -3,36 +3,36 @@ import MediaQuery from "react-responsive";
 
 function Navitems({ Links }) {
   return (
-      <div className="mzp-c-navigation-items" id="navigation-demo">
-        {/* show github link only in PC view */}
-        <MediaQuery query="(min-width: 768px)">
-          <div className="mzp-c-navigation-download">
-            <div className="mzp-c-button-download-container">
-              <a
-                href="https://github.com/CouncilDataProject"
-                className="mzp-c-button mzp-t-product mzp-t-product mzp-t-secondary mzp-t-md"
-              >
-                GitHub
-              </a>
-            </div>
+    <div className="mzp-c-navigation-items" id="navigation-demo">
+      {/* show github link only in PC view */}
+      <MediaQuery query="(min-width: 768px)">
+        <div className="mzp-c-navigation-download">
+          <div className="mzp-c-button-download-container">
+            <a
+              href="https://github.com/CouncilDataProject"
+              className="mzp-c-button mzp-t-product mzp-t-product mzp-t-secondary mzp-t-md"
+            >
+              GitHub
+            </a>
           </div>
-        </MediaQuery>
-        <div className="mzp-c-navigation-container" aria-expanded="false">
-          <nav className="mzp-c-menu mzp-is-basic">
-            <ul className="mzp-c-menu-category-list">
-              {Links.map((item) => {
-                return (
-                  <li className="mzp-c-menu-category">
-                    <a className="mzp-c-menu-title" href={item.to}>
-                      {item.name}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
         </div>
+      </MediaQuery>
+      <div className="mzp-c-navigation-container" aria-expanded="false">
+        <nav className="mzp-c-menu mzp-is-basic">
+          <ul className="mzp-c-menu-category-list">
+            {Links.map((item) => {
+              return (
+                <li className="mzp-c-menu-category">
+                  <a className="mzp-c-menu-title" href={item.to}>
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
+    </div>
   );
 }
 
@@ -45,19 +45,14 @@ export default function Navigation({ NavLinks }) {
       <div id="main-nav" className="mzp-c-navigation mzp-is-sticky">
         <div className="mzp-c-navigation-l-content">
           <div className="mzp-c-navigation-container">
-            <div className="mzp-c-navigation-logo">
-              <a
-                style={{
-                  marginTop: "0px",
-                  height: "42px",
-                  width: "42px",
-                  backgroundImage: 'url("favicon-transparent.png")',
-                  backgroundSize: "40px 40px",
-                }}
-                href="#top"
-              >
-                CDP logo
-              </a>
+            <div
+              className="cdp-icon-black-bg-transparent-size-64"
+              style={{
+                float: "left",
+                marginTop: "12px",
+              }}
+            >
+              <a href="#top">CDP logo</a>
             </div>
             {/* mobile view */}
             <MediaQuery query="(max-width: 767px)">
