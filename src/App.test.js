a41ac5375c150bebf4733771eba78ed82563dd66
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-12-01 21:25:32 -0800

    Fix render test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/City of Seattle, Washington/i);
+  const linkElement = screen.getByText(/King County, Washington/i);
   expect(linkElement).toBeInTheDocument();
 });
