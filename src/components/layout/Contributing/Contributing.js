8181f9f514b93f6fa69b48b97151684896667128
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-03-10 09:06:19 -0800

    Update contributing list

--- src/components/layout/Contributing/Contributing.js
+++ src/components/layout/Contributing/Contributing.js
@@ -113,10 +113,11 @@ export default function Contributing() {
                 </a>
               </dt>
               <dd>
-                A collection of utilities and in-progress or actively maintained 
-                CDP instance event scrapers. The purpose of this library is to 
-                help new CDP instance maintainers have a quick plethora of examples 
-                for getting started on developing their event scraper functions.
+                A collection of utilities and in-progress or actively maintained
+                CDP instance event scrapers. The purpose of this library is to
+                help new CDP instance maintainers have a quick plethora of
+                examples for getting started on developing their event scraper
+                functions.
               </dd>
 
               <dt>
@@ -133,68 +134,129 @@ export default function Contributing() {
               </dd>
             </dl>
             <h3>Contributors</h3>
-            <p>
-              Council Data Project was created by{" "}
-              <a className="cdp-link" href="https://jacksonmaxfield.github.io/">
-                Jackson Maxfield Brown
-              </a>{" "}
-              and{" "}
-              <a className="cdp-link" href="https://nicweber.info/">
-                Nic Weber
-              </a>
-              .
-            </p>
-            <p>
-              There have been major contributions from:{" "}
-              <a className="cdp-link" href="https://github.com/tohuynh">
-                To Huynh
-              </a>{" "}
-              (Full Stack Development),{" "}
-              <a className="cdp-link" href="https://www.isaacna.com/">
-                Isaac Na
-              </a>{" "}
-              (Backend Development),{" "}
-              <a className="cdp-link" href="https://hawkticehurst.com/">
-                Hawk Ticehurst
-              </a>{" "}
-              (Frontend Development),{" "}
-              <a
-                className="cdp-link"
-                href="https://www.linkedin.com/in/brianjledbetter/"
-              >
-                Brian Ledbetter
-              </a>{" "}
-              (Frontend Development),{" "}
-              <a className="cdp-link" href="https://www.katlynmfgreene.com/">
-                Katlyn M.F. Greene
-              </a>{" "}
-              (UX Research),{" "}
-              <a className="cdp-link" href="https://www.emilygilles.design/">
-                Emily Gilles
-              </a>{" "}
-              (UX Design),{" "}
-              <a
-                className="cdp-link"
-                href="https://www.linkedin.com/in/madeleine-farrer-14a1a3118/"
-              >
-                Madeleine Farrer
-              </a>{" "}
-              (Logo Design),{" "}
-              <a
-                className="cdp-link"
-                href="https://github.com/Shak2000"
-              >
-                Shak Ragoler
-              </a>{" "}
-              (Backend Developer), and{" "}
-              <a
-                className="cdp-link"
-                href="https://www.linkedin.com/in/sarah-liu-28180b1a4/"
-              >
-                Sarah Liu
-              </a>{" "}
-              (Product Manager).
-            </p>
+            <p>Council Data Project was created by:</p>
+            <ul>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://jacksonmaxfield.github.io"
+                >
+                  Jackson Maxfield Brown (Project Lead and Backend Development)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://nicweber.info/">
+                  Nic Weber (Project Lead and Advising)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://github.com/tohuynh">
+                  To Huynh (Full Stack Development)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://www.isaacna.com/">
+                  Isaac Na (Backend Development)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://hawkticehurst.com/">
+                  Hawk Ticehurst (Frontend Development)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/brianjledbetter/"
+                >
+                  Brian Ledbetter (Frontend Development)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://www.katlynmfgreene.com/">
+                  Katlyn M.F. Greene (UX Research)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://www.emilygilles.design/">
+                  Emily Gilles (UX Design)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/madeleine-farrer-14a1a3118/"
+                >
+                  Madeleine Farrer (Graphic Design)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://github.com/Shak2000">
+                  Shak Ragoler (Backend Development)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/sarah-liu-28180b1a4/"
+                >
+                  Sarah Liu (Product Management)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/sungwcho/"
+                >
+                  Sung Cho (Backend Development)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/neha-s-bb2605189/"
+                >
+                  Neha Singh (UX Design)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/kristopher-smith-/"
+                >
+                  Kris Smith (Data Science)
+                </a>
+              </li>
+              <li>
+                <a className="cdp-link" href="https://melanie-white.com/">
+                  Melanie White (UX Design)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/adrian-macdonald"
+                >
+                  Adrian MacDonald (Product Design)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/piasteffes/"
+                >
+                  Pia Steffes (UX Design)
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cdp-link"
+                  href="https://www.linkedin.com/in/dale-harvey-husband-977526a5/"
+                >
+                  Dale Harvey Husband (UX Design)
+                </a>
+              </li>
+            </ul>
             <p>
               Much of our second prototype can be attributed to the volunteers
               from{" "}
