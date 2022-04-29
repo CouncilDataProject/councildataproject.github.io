c187505ba8a0e50cd915158c4ab422964dfa0f0e
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-04-28 17:39:34 -0700

    Start to update Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import './Hero.css';
 
 export default function Hero() {
   return (
-    <section
-      id="hero"
-      className="mzp-c-hero mzp-has-image cdp-bg-dark-purple cdp-white"
-    >
-      <div className="mzp-l-content mzp-t-content-xl">
-        <div className="mzp-c-hero-body">
-          <h1 className="mzp-c-hero-title">
-            A search engine for local government.
-          </h1>
-        </div>
+    <section id="hero-container">
+      <div id="hero-content-container" class="mzp-c-hero-body">
+        <h1 class="cdp-h1 mzp-u-title-xl">
+          A search engine for local government.
+        </h1>
+        {/* <HeroButton buttonText="Select your location" href="#locations" /> */}
       </div>
-
-      <div className="mzp-c-hero-image">
-        <img
-          src="imgs/public-speaker-light-purple.svg"
-          alt="public speaker art"
-        />
-      </div>
+      <img src="imgs/cdp-hero.png" alt="" />
     </section>
+    // <section id="hero" className="hero-container">
+    //   <div className="mzp-l-content mzp-t-content-xl">
+    //     <div className="mzp-c-hero-body">
+    //       <h1 className="mzp-c-hero-title">
+    //         A search engine for local government.
+    //       </h1>
+    //     </div>
+    //   </div>
+    //   <div className="mzp-c-hero-image">
+    //     <img src="imgs/cdp-hero.png" alt="public speaker art" />
+    //   </div>
+    // </section>
   );
 }
