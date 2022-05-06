d0cd1a4848dd333e3daa0f13212ce27615ffa0e9
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-05-05 22:34:08 -0700

    Update nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,125 @@
-import React, { useState } from 'react';
-import MediaQuery from 'react-responsive';
+import React, { useEffect } from 'react';
+// import '@mozilla-protocol/core/protocol/js/protocol-menu.js';
+
+export function Navigation() {
+  const navLinks = [
+    {
+      linkText: 'About',
+      linkUrl: '#about',
+    },
+    {
+      linkText: 'Locations',
+      linkUrl: '#locations',
+    },
+    {
+      linkText: 'Contributing',
+      linkUrl: '#contributing',
+    },
+  ];
+
+  // useEffect(() => {
+  //   Mzp.Navigation.init({
+  //     onNavOpen: function () {
+  //       console.log('Navigation opened');
+  //     },
+  //     onNavClose: function () {
+  //       console.log('Navigation closed');
+  //     },
+  //   });
+  // }, []);
+
+  return (
+    <div className="mzp-c-navigation mzp-is-sticky">
+      <div className="mzp-c-navigation-l-content">
+        <div className="mzp-c-navigation-container">
+          <button
+            className="mzp-c-navigation-menu-button"
+            type="button"
+            aria-controls="navigation-menu">
+            Menu
+          </button>
+          <NavLogo />
+          <NavItems links={navLinks} />
+        </div>
+      </div>
+    </div>
+    // <section className="mzp-c-navigation">
+    //   <div className="mzp-c-navigation-l-content">
+    //     <div className="mzp-c-navigation-container">
+    //       <NavLogo />
+    //       {/* desktop view */}
+    //       <MediaQuery query="(min-width: 768px)">
+    //         <NavItems links={links} />
+    //       </MediaQuery>
+    //       {/* mobile view */}
+    //       <MediaQuery query="(max-width: 767px)">
+    //         <button
+    //           className="mzp-c-navigation-menu-button"
+    //           style={{ display: 'inline' }}
+    //           type="button"
+    //           onClick={() => {
+    //             if (isMenuOpen) {
+    //               setIsMenuOpen(false);
+    //             } else {
+    //               setIsMenuOpen(true);
+    //             }
+    //           }}>
+    //           Menu
+    //         </button>
+    //         {isMenuOpen ? <NavItems links={links} /> : <></>}
+    //       </MediaQuery>
+    //     </div>
+    //   </div>
+    // </section>
+  );
+}
+
+function NavLogo() {
+  return (
+    <section
+      className="mzp-c-navigation-logo"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+      <span
+        className="cdp-icon-black-bg-transparent-size-256"
+        style={{
+          maxWidth: '42px',
+          maxHeight: '42px',
+        }}></span>
+      <p
+        style={{
+          margin: '0',
+          fontSize: '18px',
+          fontWeight: 'bold',
+        }}>
+        Council Data Project
+      </p>
+    </section>
+  );
+}
 
 function NavItems({ links }) {
   return (
-    <div className="mzp-c-navigation-items" id="navigation-demo">
-      {/* show github link only in PC view */}
-      <MediaQuery query="(min-width: 768px)">
-        <div className="mzp-c-navigation-download">
-          <div className="mzp-c-button-download-container">
-            <a
-              href="https://github.com/CouncilDataProject"
-              className="mzp-c-button mzp-t-product mzp-t-product mzp-t-secondary mzp-t-md"
-            >
-              GitHub
-            </a>
-          </div>
+    <div className="mzp-c-navigation-items" id="navigation-menu">
+      <div className="mzp-c-navigation-download">
+        <div className="mzp-c-button-download-container">
+          <a
+            href="#"
+            className="mzp-c-button mzp-t-product mzp-t-product mzp-t-secondary mzp-t-md">
+            GitHub
+          </a>
         </div>
-      </MediaQuery>
-      <div className="mzp-c-navigation-container" aria-expanded="false">
+      </div>
+      <div className="mzp-c-navigation-menu">
         <nav className="mzp-c-menu mzp-is-basic">
           <ul className="mzp-c-menu-category-list">
-            {links.map((item) => {
+            {links.map((link) => {
               return (
-                <li className="mzp-c-menu-category">
-                  <a className="mzp-c-menu-title" href={item.to}>
-                    {item.name}
+                <li className="mzp-c-menu-category" key={link.linkUrl}>
+                  <a className="mzp-c-menu-title" href={link.linkUrl}>
+                    {link.linkText}
                   </a>
                 </li>
               );
@@ -36,51 +131,36 @@ function NavItems({ links }) {
   );
 }
 
-export default function Navigation({ links }) {
-  // state to show menu in mobile view
-  const [mmenu, setMmenu] = useState(false);
-
-  return (
-    <>
-      <div id="main-nav" className="mzp-c-navigation mzp-is-sticky">
-        <div className="mzp-c-navigation-l-content">
-          <div className="mzp-c-navigation-container">
-            <div
-              className="cdp-icon-black-bg-transparent-size-256"
-              style={{
-                float: 'left',
-                marginTop: '12px',
-                maxHeight: '64px',
-                maxWidth: '64px',
-              }}
-            >
-              <a href="#top">CDP logo</a>
-            </div>
-            {/* mobile view */}
-            <MediaQuery query="(max-width: 767px)">
-              <button
-                className="mzp-c-navigation-menu-button"
-                style={{ display: 'inline' }}
-                type="button"
-                onClick={() => {
-                  if (mmenu) {
-                    setMmenu(false);
-                  } else {
-                    setMmenu(true);
-                  }
-                }}
-              >
-                Menu
-              </button>
-              {mmenu ? <NavItems links={links} /> : <></>}
-            </MediaQuery>
-            {/* PC screen view */}
-            <MediaQuery query="(min-width: 768px)">
-              <NavItems links={links} />
-            </MediaQuery>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+// function NavItems({ links }) {
+//   return (
+//     <div className="mzp-c-navigation-items">
+//       <div className="mzp-c-navigation-container" aria-expanded="false">
+//         <nav className="mzp-c-menu mzp-is-basic">
+//           <ul className="mzp-c-menu-category-list">
+//             {links.map((item) => {
+//               return (
+//                 <li className="mzp-c-menu-category">
+//                   <a className="mzp-c-menu-title" href={item.to}>
+//                     {item.name}
+//                   </a>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </nav>
+//       </div>
+//       {/* show github link only in desktop view */}
+//       <MediaQuery query="(min-width: 768px)">
+//         <div className="mzp-c-navigation-download">
+//           <div className="mzp-c-button-download-container">
+//             <a
+//               href="https://github.com/CouncilDataProject"
+//               className="mzp-c-button mzp-t-product mzp-t-product mzp-t-secondary mzp-t-md">
+//               GitHub
+//             </a>
+//           </div>
+//         </div>
+//       </MediaQuery>
+//     </div>
+//   );
+// }
