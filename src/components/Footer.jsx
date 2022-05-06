1d6b147ffdb5ff8c8048e8b69fbf23f67abfbdcc
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-05-05 22:32:49 -0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 
-export default function Footer() {
+export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <>
-      <footer className="mzp-c-footer">
-        <div className="mzp-l-content">
-          <nav className="mzp-c-footer-secondary">
-            <div className="mzp-c-footer-legal">
-              <p>
-                Portions of this content are ©2017–2021 by individual Council
-                Data Project contributors. Content available under{' '}
-                <a
-                  rel="license"
-                  href="https://github.com/CouncilDataProject/councildataproject.github.io/blob/master/LICENSE"
-                >
-                  MIT License
-                </a>
-                .
-              </p>
-              <p>
-                Styled using{' '}
-                <a rel="external" href="https://protocol.mozilla.org/">
-                  Mozilla Protocol
-                </a>
-                . Artwork provided by{' '}
-                <a rel="external" href="https://undraw.co/">
-                  unDraw
-                </a>
-                .
-              </p>
-            </div>
-          </nav>
-        </div>
-      </footer>
-    </>
+    <footer className="mzp-c-footer">
+      <div className="mzp-l-content">
+        <nav className="mzp-c-footer-secondary">
+          <div className="mzp-c-footer-legal">
+            <p className="mzp-c-footer-license">
+              Portions of this content are ©2017-{currentYear} by individual
+              Council Data Project contributors. Content available under{' '}
+              <a
+                rel="license"
+                href="https://github.com/CouncilDataProject/councildataproject.github.io/blob/master/LICENSE">
+                MIT License
+              </a>
+              .
+            </p>
+            <p>
+              Styled using{' '}
+              <a rel="external" href="https://protocol.mozilla.org/">
+                Mozilla Protocol
+              </a>
+              . Artwork provided by{' '}
+              <a rel="external" href="https://undraw.co/">
+                unDraw
+              </a>
+              . Icons provided by{' '}
+              <a rel="external" href="https://ionic.io/ionicons">
+                ionicons
+              </a>
+              .
+            </p>
+          </div>
+        </nav>
+      </div>
+    </footer>
   );
 }
